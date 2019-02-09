3f42a229087be0499688ebcab77fc05e56ef12c3
Jacek Wesołowski <jacqu25@yahoo.com>
2019-02-09 10:52:43 +0100

    battle create endpoint

---
+++ src/AppBundle/Form/Model/BattleModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form\Model;
+
+use AppBundle\Entity\Programmer;
+use AppBundle\Entity\Project;
+
+class BattleModel
+{
+    private $project;
+
+    private $programmer;
+
+    public function getProject()
+    {
+        return $this->project;
+    }
+
+    public function setProject(Project $project)
+    {
+        $this->project = $project;
+
+        return $this;
+    }
+
+    public function getProgrammer()
+    {
+        return $this->programmer;
+    }
+
+    public function setProgrammer(Programmer $programmer)
+    {
+        $this->programmer = $programmer;
+
+        return $this;
+    }
+}
